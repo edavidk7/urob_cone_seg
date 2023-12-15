fc9f41d85dae48db5206f799508baf9637a865f4
Roman <sheeproman@gmail.com>
2023-12-16 00:03:19 +0100

    focal loss

--- utils/losses.py
+++ utils/losses.py
@@ -3,5 +3,5 @@ from torch.nn.functional import cross_entropy
 
 def focal_loss(input, target, gamma=2):
     logp = -cross_entropy(input, target, reduction='none')
-    loss = -(1 - torch.exp(logp)) ** self.gamma * logp
+    loss = -(1 - torch.exp(logp)) ** gamma * logp
     return loss.mean()
