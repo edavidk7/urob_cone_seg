7690592a2e6bd1b68820a3dae6587a93e7a0d77a
David Korčák <korcadav@fel.cvut.cz>
2023-12-20 00:31:36 +0100

    Fixed segment vision log

--- segment_vision_log.py
+++ segment_vision_log.py
@@ -5,14 +5,15 @@ import matplotlib.pyplot as plt
 import numpy as np
 from pathlib import Path
 from tqdm import tqdm
-
+import time
 # from train_config import eval_T, config
 from train_config import *
 from utils.tools import mask_tensor_to_rgb, image_tensor_to_rgb
 
-def setup_model():
+
+def setup_model(best_path):
     model = config["model_type"](**config["model_kwargs"]).to(config["device"])
-    state_dict = torch.load("best/best_weights.pt", map_location=config["device"])
+    state_dict = torch.load(Path(best_path) / "best/best_weights.pt", map_location=config["device"])
     for key in list(state_dict.keys()):
         if key.startswith("module."):
             state_dict[key[7:]] = state_dict.pop(key)
@@ -20,25 +21,29 @@ def setup_model():
     model.eval()
     return model
 
+
 def video_iterator(path):
     video = cv2.VideoCapture(str(path))
     while True:
         ret, frame = video.read()
-        if not ret: break
+        if not ret:
+            break
         yield frame
     video.release()
 
+
 def video_writer(path, fps, size):
-    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-    return cv2.VideoWriter(str(path), fourcc, fps, (size[1]*2, size[0]))
+    fourcc = cv2.VideoWriter_fourcc(*'h264')
+    return cv2.VideoWriter(str(path), fourcc, fps, (size[1] * 2, size[0]), True)
+
 
 def segment_vision_log(args):
-    vid = video_iterator(args.path)
+    vid = video_iterator(args.video_path)
     with torch.no_grad():
-        model = setup_model()
-
+        model = setup_model(args.model_path)
         # make a bar from start_frame to end_frame
-        bar = tqdm(total=args.end-args.start)
+        bar = tqdm(total=args.end - args.start)
+        time_sum = 0
         for i, frame in enumerate(vid):
             if i == 0:
                 size = (frame.shape[0], frame.shape[1])
@@ -47,34 +52,43 @@ def segment_vision_log(args):
             if i < args.start:
                 continue
 
+            # Convert frame to tensor
             image_tensor = torch.from_numpy(frame).permute(2, 0, 1).float()
             image_tensor = image_tensor[[2, 1, 0]]
-            empty_mask = torch.zeros((6,image_tensor.shape[1], image_tensor.shape[2]))
+            # Normalize and resize
+            empty_mask = torch.zeros((6, image_tensor.shape[1], image_tensor.shape[2]))
             image_tensor = eval_T((image_tensor, empty_mask))[0].to(config["device"])
+            # Predict
+            start = time.perf_counter_ns()
             preds = model(image_tensor.unsqueeze(0))[0].detach().cpu()
-            mask = mask_tensor_to_rgb(preds)
+            end = time.perf_counter_ns()
+            mask = cv2.cvtColor(mask_tensor_to_rgb(preds), cv2.COLOR_RGB2BGR)
+            #  Concatenate
             final_image = np.concatenate((frame, mask), axis=1)
-
             if args.display:
-                cv2.imshow("image",final_image)
+                cv2.imshow("image", final_image)
                 cv2.waitKey(1)
-
+            delta = (end - start) / 1e6
+            bar.set_description(f"FPS: {1000 / delta:.2f} on {config['device']}")
             writer.write(final_image)
             bar.update(1)
-
             if i == args.end:
                 break
+
         writer.release()
         bar.close()
 
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--path", help="Path to log file", required=True)
+    parser.add_argument("--model_path", help="Path to model file", required=True)
+    parser.add_argument("--video_path", help="Path to source video file", required=True)
     parser.add_argument("--out", default=Path("./results.mp4"), help="Path to output file")
     parser.add_argument("--start", default=300, type=int, help="Start frame")
-    parser.add_argument("--end", default=800, type=int, help="End frame")
+    parser.add_argument("--end", default=2000, type=int, help="End frame")
     parser.add_argument("--fps", default=30, type=int, help="FPS of output video")
     parser.add_argument("--display", action="store_true", help="Display video")
+    parser.add_argument("--verbose", action="store_true", help="Display video")
     args = parser.parse_args()
 
     segment_vision_log(args)
--- train_config.py
+++ train_config.py
@@ -7,34 +7,35 @@ from torch.optim import SGD, Adam, AdamW
 from torch.optim.lr_scheduler import CosineAnnealingLR, StepLR, ReduceLROnPlateau
 
 class_color_jitter = {
-    0: {"hue": 0.5, "saturation": 0.5},
-    1: {"hue": 0.5, "saturation": 0.8},
-    2: {"hue": 0.5, "saturation": 0.8},
-    3: {"hue": 0.5, "saturation": 0.8},
-    4: {"hue": 0.5, "saturation": 0.8},
-    5: {"hue": 0.5, "saturation": 0.8},
+    0: {"hue": (-0.3, 0.3), "saturation": (0.8, 1.2), "brightness": (0.8, 1.2)},
+    1: {"hue": (-0.5, 0.5), "saturation": (0.3, 1.8), "brightness": (0.5, 1.7)},
+    2: {"hue": (-0.5, 0.5), "saturation": (0.3, 1.8), "brightness": (0.5, 1.7)},
+    3: {"hue": (-0.5, 0.5), "saturation": (0.3, 1.8), "brightness": (0.5, 1.7)},
+    4: {"hue": (-0.5, 0.5), "saturation": (0.3, 1.8), "brightness": (0.5, 1.7)},
+    5: {"hue": (-0.5, 0.5), "saturation": (0.3, 1.8), "brightness": (0.5, 1.7)},
 }
 
 train_T = transforms.Compose(
     [
-        Normalize(),
-        RandomCropWithMask(size=(512, 512), skip_smaller=True),
+        ClasswiseColorJitter(class_color_jitter),
         RandomHorizontalFlipWithMask(0.5),
-        # RandomAffineWithMask(degrees=10, translate=(0.01, 0.01)),
-        # RandomRotationWithMask(degrees=5),
-        # ClasswiseColorJitter(class_color_jitter)
+        RandomAffineWithMask(degrees=20, translate=(0.1, 0.1)),
+        ResizeWithMask(size=(720, 1280), antialias=True),
+        Normalize(),
     ])
 
-eval_T = transforms.Compose(
-    [Normalize(), ResizeWithMask(size=(720, 1280), antialias=True)])
+eval_T = transforms.Compose([
+    ResizeWithMask(size=(720, 1280), antialias=True),
+    Normalize(),
+])
 
 config = {
     # Model setup
     "model_type": MobileV3Small,
-    "model_kwargs": {"num_classes": 6, "num_filters": 128, "use_aspp": True},
+    "model_kwargs": {"num_classes": 6, "num_filters": 256, "use_aspp": True},
     # Optimizer setup
     "optim_type": Adam,
-    "optim_kwargs": {"lr": 0.001, "weight_decay": 0.025},
+    "optim_kwargs": {"lr": 0.001, "weight_decay": 0.000},
     # Scheduler setup
     "scheduler_type": ReduceLROnPlateau,
     "scheduler_kwargs": {"mode": "min", "factor": 0.5, "patience": 2, "verbose": True},
@@ -42,7 +43,7 @@ config = {
     # Loss setup
     "loss_fn": CrossEntropyLoss,
     "loss_kwargs": {"reduction": "none"},
-    "use_weighted_loss": True,  # Gets added to kwargs later
+    "use_weighted_loss": False,  # Gets added to kwargs later
     "loss_weight_fn": ClassDistrToWeight.sqrt_one_minus,
     # Data setup
     "num_classes": 6,
@@ -54,10 +55,10 @@ config = {
     "data_path": "fsoco_segmentation_processed",
     "imdir": "imgs",
     "maskdir": "masks",
-    "num_epochs": 10,
+    "num_epochs": 40,
     "device": "mps",
-    "train_loader_kwargs": {"pin_memory": True, "persistent_workers": True, "shuffle": True, "num_workers": 2, "batch_size": 2},
-    "eval_loader_kwargs": {"pin_memory": True, "persistent_workers": True, "shuffle": False, "num_workers": 2, "batch_size": 2},
+    "train_loader_kwargs": {"pin_memory": True, "persistent_workers": True, "shuffle": True, "num_workers": 8, "batch_size": 16},
+    "eval_loader_kwargs": {"pin_memory": True, "persistent_workers": True, "shuffle": False, "num_workers": 8, "batch_size": 16},
     "dataparallel": True,
     # Logging and evaluation setup
     "save_path": "./train_results",
